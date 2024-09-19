578e496effe187aa502866ebba132f6b409593ad
struk77 <andrey.struk@gmail.com>
2024-09-19 09:45:07 +0200

    add debug env

--- app.py
+++ app.py
@@ -109,4 +109,5 @@ def upload_file():
 # Main function
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    debug = os.environ.get("DEBUG") == "TRUE"
+    app.run(host="127.0.0.1", port=port, debug=debug)
